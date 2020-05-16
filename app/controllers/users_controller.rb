87f9b7a90a785ae18e1de13cf9e0c6c2894af667
gshah2020 <gaurang.r.shah@gmail.com>
2020-05-16 16:59:22 +0000

    feat: new user form

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,37 @@
+class UsersController < ApplicationController
+  
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params) # user_params are whitelisted params our form accpects.
+    if @user.save
+      flash[:success] = "Welcome to the alpha blog #{@user.username}"
+      redirect_to articles_path
+    else
+      flash[:warning] = "Sorry there was an issue saving thisuser"
+      render 'new' #renders new user template if any errors occur
+    end
+  end
+
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+    if @user.update(user_params)
+      flash[:success] = "User #{params[:id]} updated"
+      redirect_to articles_path
+    else
+      flash[:warning] = "Sorry there was an issue updateed user: #{@user.id}"
+      render 'edit'
+    end
+  end
+
+  private
+  def user_params
+    params.require(:user).permit(:username, :email, :password)
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -6,4 +6,8 @@ Rails.application.routes.draw do
   get 'about', to: 'pages#about' 
   
   resources :articles # sets up all paths to handle our article related endpoints
+
+  get 'signup', to: 'users#new'
+  # post 'users', to: 'users#create'
+  resources :users, except: [:new]
 end
